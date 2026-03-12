8e5693e7838c8a0be403c3ce943e24b97bf0f0fd
Daniel Aronoff <daronoff@mit.edu>
2026-03-11 21:43:43 -0400

    Fix static publish script to honor Ruby version

--- scripts/build_static_pages.sh
+++ scripts/build_static_pages.sh
@@ -3,7 +3,6 @@ set -euo pipefail
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 RBENV_VERSION="${RBENV_VERSION:-3.2.3}"
-BUNDLE_BIN="/Users/danielaronoff/.rbenv/shims/bundle"
 
-"$BUNDLE_BIN" exec jekyll build -d "$ROOT_DIR/docs"
+RBENV_VERSION="${RBENV_VERSION}" /Users/danielaronoff/.rbenv/shims/bundle exec jekyll build -d "$ROOT_DIR/docs"
 touch "$ROOT_DIR/docs/.nojekyll"
